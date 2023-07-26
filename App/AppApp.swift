ac885e2af58091b85fe93fa015d91c5b8a5a8346
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-26 12:13:20 +0700

    :money_with_wings: re

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Poundpitak Amornlerdlittichai on 26/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
