d785a081cc40b8ebdc24ec3490d68c102e3b72cc
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-26 11:42:53 +0700

    :money_with_wings: first

---
+++ achitecture/achitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  achitectureApp.swift
+//  achitecture
+//
+//  Created by Poundpitak Amornlerdlittichai on 26/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct achitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
