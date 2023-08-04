18ffe111793d6e813e51b5f04adb72a169cb2675
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-08-04 15:06:53 +0700

    Revert ":money_with_wings: set home screen"
    This reverts commit f21421215f8e5d9ff99fb6d4421c2975598c4578.

--- App/Main.swift
+++ App/Main.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Main: App {
     var body: some Scene {
         WindowGroup {
-            HomeScreen()
+            ContentView()
         }
     }
 }
---
+++ App/Presentation/Scenes/Views/CustomView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomView.swift
+//  App
+//
+//  Created by Poundpitak Amornlerdlittichai on 26/7/2566 BE.
+//
+
+import Foundation
