506a67f19b81d0cbe9b5f1efb349d6a50734dde8
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-08-04 15:38:43 +0700

    :money_with_wings: update home screen && button

---
+++ App/Presentation/Scenes/ViewControllers/Button.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct ButtonComponent: View {
+    let title: String
+    let color: Color
+    let icon: String?
+    let width: CGFloat
+    let height: CGFloat
+    let radius: CGFloat
+    let size: CGFloat
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack(spacing: 8) {
+                if let iconName = icon {
+                    Image(systemName: iconName)
+                        .foregroundColor(.white)
+                }
+                
+                Text(title)
+                    .foregroundColor(.white)
+                    .font(.system(size: size))
+                    .padding()
+            }
+            .frame(width: width, height: height)
+            .background(color)
+            .cornerRadius(radius)
+        }
+    }
+}
--- App/Presentation/Scenes/Views/HomeView.swift
+++ App/Presentation/Scenes/Views/HomeView.swift
@@ -2,13 +2,28 @@ import SwiftUI
 
 struct HomeScreen: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Welcome to My App")
-                .font(.title)
-                .padding()
-            Spacer()
-
+        NavigationView {
+            VStack {
+                Spacer()
+                Text("Welcome to My App")
+                    .font(.title)
+                    .padding()
+                Spacer()
+                
+                ButtonComponent(
+                    title: "Sign In",
+                    color: .blue,
+                    icon: "person.fill",
+                    width: 200,
+                    height: 50,
+                    radius: 10,
+                    size: 18
+                ) {
+                }
+                .padding(.horizontal)
+                Spacer()
+            }
+            
         }
     }
 }
